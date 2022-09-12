b3788b665ab1521c07effc48b68da734d55db7d3
Sai Santhosh <saisantu02@Sais-MacBook-Air.local>
2022-09-12 09:37:55 +0530

    Added commitzen

--- src/header.js
+++ src/header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header= ()=>{
     return(
         <>
-        <h1 style={{backgroundColor:"pink", textAlign: "center",padding: 30, marginBottom: 50}}>Registration V4</h1>
+        <h1 style={{backgroundColor:"pink", textAlign: "center",padding: 30, marginBottom: 50}}>Registration V3</h1>
         </>
     )
 }
