bfc7f03d543eb5f0532c55c6cc7f29c689af9405
Sai Santhosh <saisantu02@Sais-MacBook-Air.local>
2022-09-12 11:06:38 +0530

    added delete cache in docker  Please enter the commit message for your changes. Lines starting

--- src/header.js
+++ src/header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header= ()=>{
     return(
         <>
-        <h1 style={{backgroundColor:"blue", textAlign: "center",padding: 30, marginBottom: 50}}>Registration V4</h1>
+        <h1 style={{backgroundColor:"red", textAlign: "center",padding: 30, marginBottom: 50}}>Registration V4</h1>
         </>
     )
 }
